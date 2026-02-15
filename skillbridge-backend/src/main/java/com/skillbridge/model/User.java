1bf3fa5d7b8e83307a58e4d835c33f640335d9c0
pavankumarkadiri <pavankumarno143@gmail.com>
2026-02-15 16:19:19 +0530

    1st milestone for backend done

--- skillbridge-backend/src/main/java/com/skillbridge/model/User.java
+++ skillbridge-backend/src/main/java/com/skillbridge/model/User.java
@@ -22,15 +22,42 @@ public class User {
     @Column(nullable = false)
     private String fullName;
 
+    @Column(nullable = false)
+    private String role;
+
+    // optional for both ngo and volunteer
+    private String location;
+
+    //For volunteer
+    private String skills;
+    private String bio;
+
+    // For NGO
+    private String organizationName;
+    private String organizationDescription;
+    private String websiteUrl;
+
+
+
+
+
     // Constructors
     public User() {}
     
-    public User(Long id, String username, String email, String password, String fullName) {
+    public User(Long id, String username, String email, String password, String fullName, String role, String location, String skills, String bio, String organizationName, String organizationDescription, String websiteUrl) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
         this.fullName = fullName;
+        this.role = role;
+        this.location = location;
+        this.skills = skills;
+        this.bio = bio;
+        this.organizationName = organizationName;
+        this.organizationDescription = organizationDescription;
+        this.websiteUrl = websiteUrl;
+
     }
 
     // Getters and Setters
@@ -73,4 +100,32 @@ public class User {
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+
+    public String getRole() { return role; }
+
+    public void setRole(String role) { this.role = role; }
+
+    public String getLocation() { return location; }
+
+    public void setLocation(String location) { this.location = location; }
+
+    public String getSkills() { return skills; }
+
+    public void setSkills(String skills) { this.skills = skills; }
+
+    public String getBio() { return bio; }
+
+    public void setBio(String bio) { this.bio = bio; }
+
+    public String getOrganizationName() { return organizationName; }
+
+    public void setOrganizationName(String organizationName) { this.organizationName = organizationName; }
+
+    public String getOrganizationDescription() { return organizationDescription; }
+
+    public void setOrganizationDescription(String organizationDescription) { this.organizationDescription = organizationDescription;  }
+
+    public String getWebsiteUrl() { return websiteUrl; }
+
+    public void setWebsiteUrl(String websiteUrl) { this.websiteUrl = websiteUrl; }
 }
